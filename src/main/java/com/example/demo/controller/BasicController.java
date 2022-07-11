a9db781bbc620a7dd94c6bc6eb69ef473505e683
sujin <atena9604@naver.com>
2022-07-11 23:40:23 +0900

    [feat] port 8081로 수정 및 controller uri 수정

--- src/main/java/com/example/demo/controller/BasicController.java
+++ src/main/java/com/example/demo/controller/BasicController.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BasicController {
-    @GetMapping("/sample")
+    @PutMapping("/saveList")
     public @ResponseBody String test() {
 
         return "success";
