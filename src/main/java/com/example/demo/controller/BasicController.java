c3d43c76cfe0d68d19cc61598dcd6a472db850e3
sujin <atena9604@naver.com>
2022-07-19 00:36:00 +0900

    [feat] 값 가져오는 부분 추가

--- src/main/java/com/example/demo/controller/BasicController.java
+++ src/main/java/com/example/demo/controller/BasicController.java
@@ -3,6 +3,8 @@ import com.example.demo.data.entity.ListEntitiy;
 import com.example.demo.data.repository.ListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -13,13 +15,19 @@ public class BasicController {
 
     @CrossOrigin("*")
     @PutMapping("/saveList")
-    public String test(@RequestBody Map<String, Object> list) {
-        listRepository.findAll();
+    public String saveList(@RequestBody Map<String, Object> list) {
         ListEntitiy listOfEntity = new ListEntitiy();
-
         listOfEntity.setContent(list.get("newContent").toString());
+
         listRepository.save(listOfEntity);
 
         return "success";
+    }
+
+    @CrossOrigin("*")
+    @GetMapping("/getList")
+    public List<ListEntitiy> getList() {
+
+        return listRepository.findAll();
     }
 }
